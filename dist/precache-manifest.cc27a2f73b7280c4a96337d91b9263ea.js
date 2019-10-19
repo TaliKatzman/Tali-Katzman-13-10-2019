54f90fd6501583e58907daa499b267202144cd0e
Tali Katzman <tali.katzman@gmail.com>
2019-10-20 00:10:43 +0300

    added dist

---
+++ dist/precache-manifest.cc27a2f73b7280c4a96337d91b9263ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff2307667b26eb664d2d2886b43c9174",
+    "url": "/io/weather-app-react/index.html"
+  },
+  {
+    "revision": "4177e31574695dd8219d",
+    "url": "/io/weather-app-react/static/css/main.ffe46e6c.chunk.css"
+  },
+  {
+    "revision": "434aa1d7345c8fdf81e4",
+    "url": "/io/weather-app-react/static/js/2.64d5582f.chunk.js"
+  },
+  {
+    "revision": "4177e31574695dd8219d",
+    "url": "/io/weather-app-react/static/js/main.24581fd7.chunk.js"
+  },
+  {
+    "revision": "98f8d8048e1ea95fba37",
+    "url": "/io/weather-app-react/static/js/runtime-main.76fec002.js"
+  },
+  {
+    "revision": "695f8904a41710509f96f7fe37bf4390",
+    "url": "/io/weather-app-react/static/media/city.695f8904.jpg"
+  },
+  {
+    "revision": "439dc8250597facdc690aaf05ad7ca43",
+    "url": "/io/weather-app-react/static/media/spinner.439dc825.gif"
+  }
+]);
