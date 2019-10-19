170cfd95eb25d9bcdc1e7d67fcc3b798671a0511
Tali Katzman <tali.katzman@gmail.com>
2019-10-20 00:37:10 +0300

    Updates

---
+++ precache-manifest.2d250b7df5720b65d0cc94beabe3210f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e056489cc1bc94fe07973a787368c08c",
+    "url": "/weather-app-react/index.html"
+  },
+  {
+    "revision": "0a812cf7c8a590a91f11",
+    "url": "/weather-app-react/static/css/main.d781cc7d.chunk.css"
+  },
+  {
+    "revision": "434aa1d7345c8fdf81e4",
+    "url": "/weather-app-react/static/js/2.64d5582f.chunk.js"
+  },
+  {
+    "revision": "0a812cf7c8a590a91f11",
+    "url": "/weather-app-react/static/js/main.24581fd7.chunk.js"
+  },
+  {
+    "revision": "2715dfb897a74d7ec16e",
+    "url": "/weather-app-react/static/js/runtime-main.48eace53.js"
+  },
+  {
+    "revision": "695f8904a41710509f96f7fe37bf4390",
+    "url": "/weather-app-react/static/media/city.695f8904.jpg"
+  },
+  {
+    "revision": "439dc8250597facdc690aaf05ad7ca43",
+    "url": "/weather-app-react/static/media/spinner.439dc825.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/io/weather-app-react/precache-manifest.cc27a2f73b7280c4a96337d91b9263ea.js"
+  "/weather-app-react/precache-manifest.2d250b7df5720b65d0cc94beabe3210f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/io/weather-app-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/weather-app-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
